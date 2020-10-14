5c6be89d080e34e076b8cf6b52e8e5512f180e22
mayali123 <59428484+mayali123@users.noreply.github.com>
2020-10-14 19:21:14 +0800

    Update 复习线性表.cpp

--- 复习/复习线性表.cpp
+++ 复习/复习线性表.cpp
@@ -7,31 +7,31 @@
 #define MAXSIZE 20
 #define Status int
 #define ElemType int  
-typedef struct List{
+typedef struct List {
 	ElemType data[MAXSIZE];
 	int length;
-}L; 
+}L;
 L* InitList()
 {
-	L*list=(L*)malloc(sizeof(L));
-	list->length=0; 
+	L* list = (L*)malloc(sizeof(L));
+	list->length = 0;
 }
-void CreateList(L*list,ElemType *e,int len)
+void CreateList(L* list, ElemType* e, int len)
 {
-	for(int i=0;i<len;i++)
+	for (int i = 0; i < len; i++)
 	{
 		list->data[i] = e[i];
-	} 
+	}
 	list->length = len;
 }
 void Show(L* list)
 {
 	printf("\n");
 	for (int i = 0; i < list->length; i++)
-		printf("%d ",list->data[i]);
+		printf("%d ", list->data[i]);
 	printf("\n");
 }
-int ListEmpty(L*list)
+int ListEmpty(L* list)
 {
 	return list->length == 0;
 }
@@ -41,7 +41,7 @@ Status GetElem(L* list, int index, ElemType* e)
 		return ERROR;
 	for (int i = 0; i < list->length; i++)
 	{
-		if (i+1 == index)
+		if (i + 1 == index)
 			*e = list->data[i];
 	}
 	return OK;
@@ -55,7 +55,7 @@ Status LocateElem(L* list, ElemType e)
 	}
 	return FALSE;
 }
-Status ElemInsert(L* list,int index, ElemType e)
+Status ElemInsert(L* list, int index, ElemType e)
 {
 	if (index<1 || index>list->length + 1)
 		return ERROR;
@@ -63,37 +63,37 @@ Status ElemInsert(L* list,int index, ElemType e)
 		return ERROR;
 	if (index <= list->length + 1)
 	{
-		
-		for (int i = list->length - 1; i >=index-1; i--)
+
+		for (int i = list->length - 1; i >= index - 1; i--)
 		{
 			list->data[i + 1] = list->data[i];
 		}
 	}
-	list->data[index-1] = e;
+	list->data[index - 1] = e;
 	list->length++;
 	return OK;
 }
-Status ElemDel(L* list, int index, ElemType *e)
+Status ElemDel(L* list, int index, ElemType* e)
 {
 	if (index<1 || index>list->length)
 		return ERROR;
 	int i = index - 1;
 	*e = list->data[i];
-	for (; i+1 < list->length; i++)
+	for (; i + 1 < list->length; i++)
 		list->data[i] = list->data[i + 1];
 	return OK;
 }
 int main()
 {
 	L* list = InitList();
-	int E[6] = {1,2,3,4,5,6},e;
+	int E[6] = { 1,2,3,4,5,6 }, e;
 	CreateList(list, E, 6);
 	Show(list);
-	GetElem(list,6,&e);
-	printf("%d\n",e);
-	printf("%dÔÚ%d\n",e,LocateElem(list,e));
-	ElemInsert(list,2,10);
+	GetElem(list, 6, &e);
+	printf("%d\n", e);
+	printf("%dåœ¨%d\n", e, LocateElem(list, e));
+	ElemInsert(list, 2, 10);
 	Show(list);
 	ElemDel(list, 1, &e);
-	printf("É¾³ýÁË%d\n",e);
+	printf("åˆ é™¤äº†%d\n", e);
 }
