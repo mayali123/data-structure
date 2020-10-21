bc33f50677b9f204504d129eec9f436b2bc9e6d6
mayali123 <59428484+mayali123@users.noreply.github.com>
2020-10-21 11:23:41 +0800

    Update 书上73页的题目.cpp

--- 作业/书上73页的题目.cpp
+++ 作业/书上73页的题目.cpp
@@ -11,7 +11,7 @@ typedef struct LinkList {
 	ElemType data;
 	struct LinkList* next;
 }LL;
-LL* CreateListR(ElemType* e, int len)  // Î²²å ´øÍ·½áµã
+LL* CreateListR(ElemType* e, int len)  // å°¾æ’ å¸¦å¤´ç»“ç‚¹
 {
 	LL* linklist = (LL*)malloc(sizeof(LL)), * r, * s;
 	r = linklist;
@@ -25,7 +25,7 @@ LL* CreateListR(ElemType* e, int len)  // Î²²å ´øÍ·½áµã
 	r->next = NULL;
 	return linklist;
 }
-LL* CreateListF(ElemType* e, int len) // Í·²å  ´øÍ·½áµã  È»ºóÓëÔ­Ë³ĞòÏà·´
+LL* CreateListF(ElemType* e, int len) // å¤´æ’  å¸¦å¤´ç»“ç‚¹  ç„¶åä¸åŸé¡ºåºç›¸å
 {
 	LL* linklist = (LL*)malloc(sizeof(LL)), * s;
 	linklist->next = NULL;
@@ -130,7 +130,7 @@ void ListDestroy(LL* linklist)
 void quick_middle_search(LL* linklist)
 {
 	LL* f = linklist->next, * s = f;
-	for (;f->next;)
+	for (; f->next;)
 	{
 		if (f->next->next)
 		{
@@ -140,41 +140,41 @@ void quick_middle_search(LL* linklist)
 		else
 			f = f->next;
 	}
-	printf("%d\n",s->data);
+	printf("%d\n", s->data);
 }
-int LocateElemPlus_Up(LL* linklist, ElemType e)  //ÊÊÓÃÓÚÔªËØÊÇÉıĞòµÄ
+int LocateElemPlus_Up(LL* linklist, ElemType e)  //é€‚ç”¨äºå…ƒç´ æ˜¯å‡åºçš„
 {
-	int i = 1,flog=1;  //flog ÓÃÀ´ÅĞ¶ÏÊÇ²»ÊÇµÚÒ»¸ö
+	int i = 1, flog = 1;  //flog ç”¨æ¥åˆ¤æ–­æ˜¯ä¸æ˜¯ç¬¬ä¸€ä¸ª
 	//1 3 5 7
 	LL* f = linklist->next, * s = f;
 	for (; f->next;)
 	{
 		if (f->data == e)
-		{ 
+		{
 			/*printf("f:%d\n", f->data);*/
 			return i;
 		}
-		else if(f->data>e)
-		{ 
+		else if (f->data > e)
+		{
 			if (s->data == e)
 			{
 				/*printf("s:%d\n", s->data);*/
-				if(!flog)  //µÚÒ»¸ö¾Í²»ÓÃ¼õ1ÁË ÆäËûµÄÒª¼õ
+				if (!flog)  //ç¬¬ä¸€ä¸ªå°±ä¸ç”¨å‡1äº† å…¶ä»–çš„è¦å‡
 					i--;
 				return i;
 			}
 			else
 				return NULL;
 		}
-		else{
+		else {
 			if (f->next->next)
 			{
 				s = f->next;
 				f = f->next->next;
-				i+=2;
+				i += 2;
 			}
 			else
-			{ 
+			{
 				f = f->next;
 				/*i += 1;*/
 			}
@@ -186,7 +186,7 @@ int LocateElemPlus_Up(LL* linklist, ElemType e)  //ÊÊÓÃÓÚÔªËØÊÇÉıĞòµÄ
 
 int LocateElemPlus_Down(LL* linklist, ElemType e)
 {
-	int i = 1, flog = 1;  //flog ÓÃÀ´ÅĞ¶ÏÊÇ²»ÊÇµÚÒ»¸ö
+	int i = 1, flog = 1;  //flog ç”¨æ¥åˆ¤æ–­æ˜¯ä¸æ˜¯ç¬¬ä¸€ä¸ª
 	//1 3 5 7
 	LL* f = linklist->next, * s = f;
 	for (; f->next;)
@@ -225,45 +225,65 @@ int LocateElemPlus_Down(LL* linklist, ElemType e)
 	}
 	return ListLength(linklist);
 }
+LL* transform(LL *list) // å¤´æ’  å¸¦å¤´ç»“ç‚¹  ç„¶åä¸åŸé¡ºåºç›¸å
+{ 
+	//ElemType* e;
+	//int len;
+	LL* linklist = (LL*)malloc(sizeof(LL)), * s,*p=list->next;
+	linklist->next = NULL;
+	for (; p; p=p->next)
+	{
+		s = (LL*)malloc(sizeof(LL));
+		s->data = p->data;
+		s->next = linklist->next;
+		linklist->next = s;
+	}
+	return linklist;
+}
 int main()
 {
 	int E2[] = { 1,2,3,4,5,6 }, e;
 	LL* linklistR = CreateListR(E2, 6);
 	//LL* linklistF = CreateListF(E,6);
 	show(linklistR);
-	printf("list µÄ³¤¶ÈÊÇ %d\n", ListLength(linklistR));
+	printf("list çš„é•¿åº¦æ˜¯ %d\n", ListLength(linklistR));
 	GetElem(linklistR, 2, &e);
-	printf("index=%dµÄÔªËØÊÇ%d\n", 2, e);
-	printf("%dÔÚlistµÄµÚ%d¸ö\n", 4, LocateElem(linklistR, 4));
-	printf("ÔÚindex=1²åÈëÒ»¸öÖµ\n");
+	printf("index=%dçš„å…ƒç´ æ˜¯%d\n", 2, e);
+	printf("%dåœ¨listçš„ç¬¬%dä¸ª\n", 4, LocateElem(linklistR, 4));
+	printf("åœ¨index=1æ’å…¥ä¸€ä¸ªå€¼\n");
 	ElemInsert(linklistR, 2, 9);
 	show(linklistR);
 	ElemDel(linklistR, 7, &e);
-	printf("ÔÚindex=1É¾³ıÒ»¸öÖµ%d\n", e);
+	printf("åœ¨index=1åˆ é™¤ä¸€ä¸ªå€¼%d\n", e);
 	show(linklistR);
-	printf("ÊÍ·Ålist\n");
+	printf("é‡Šæ”¾list\n");
 	ListDestroy(linklistR);
 
 	printf("\n\n");
-	ElemType E1[] = {15,12,11,9,7,5,4,2,1};
+	ElemType E1[] = { 15,12,11,9,7,5,4,2,1 };
 	LL* linklist1 = CreateListR(E1, 9);
-	printf("½µĞòµÄlistÎª ");
+	printf("é™åºçš„listä¸º ");
 	show(linklist1);
 
-	ElemType E[] = { 1,2,3 ,4,5,6,7,8,9};
-	LL* linklist  = CreateListR(E , 9);
-	printf("ÉıĞòµÄlistÎª ");
+	ElemType E[] = { 1,2,3 ,4,5,6,7,8,9 };
+	LL* linklist = CreateListR(E, 9);
+	printf("å‡åºçš„listä¸º ");
 	show(linklist);
 
-	
-	printf("²éÕÒ½µĞòµÄlistµÄÖĞ¼äÔªËØ ");
-	quick_middle_search(linklist1);
-	
 
-	int index = LocateElemPlus_Up(linklist,3);
-	printf("ÔÚÉıĞòµÄlist²éÕÒ3µÄindex=%d\n",index);
+	printf("æŸ¥æ‰¾é™åºçš„listçš„ä¸­é—´å…ƒç´  ");
+	quick_middle_search(linklist1);
+
+
+	int index = LocateElemPlus_Up(linklist, 3);
+	printf("åœ¨å‡åºçš„listæŸ¥æ‰¾3çš„index=%d\n", index);
 
 
 	int index1 = LocateElemPlus_Down(linklist1, 5);
-	printf("ÔÚ½µĞòµÄlist²éÕÒ5µÄindex=%d\n", index1);
+	printf("åœ¨é™åºçš„listæŸ¥æ‰¾5çš„index=%d\n", index1);
+
+	printf("ç¿»è½¬\n");
+	show(linklist);
+	LL *tran_list=transform(linklist);
+	show(tran_list);
 }
