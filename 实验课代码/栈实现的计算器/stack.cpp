595573a9f06f60df2c85810589a7f88953f5b896
mayali123 <59428484+mayali123@users.noreply.github.com>
2020-10-29 20:08:58 +0800

    Add files via upload

---
+++ 实验课代码/栈实现的计算器/stack.cpp
@@ -0,0 +1,127 @@
+﻿#include"struct.h"
+
+void trans(char* exp, char* postexp)
+{
+	ST* p = InitStack();
+	int k = 0;
+	char e;
+	for (int i = 0; exp[i] != '\0';)
+	{
+		switch (exp[i])
+		{
+		case '(':
+			Push(p, exp[i]);
+			i++;
+			break;
+		case ')':
+			while (1)
+			{
+				Pop(p, &e);
+				if (e == '(')
+					break;
+				postexp[k++] = e;
+			}
+			i++;
+			break;
+		case '+':
+		case '-':
+			while (!StackEmpty(p))
+			{
+				GetTop(p, &e);
+				if (e != '(')
+				{
+					Pop(p, &e);
+					postexp[k++] = e;
+				}
+				else
+					break;
+			}
+			Push(p, exp[i]);
+			i++;
+			break;
+		case '*':
+		case '/':
+			while (!StackEmpty(p))
+			{
+				GetTop(p, &e);
+				if (e == '(' || e == '+' || e == '-')
+					break;
+				Pop(p, &e);
+				postexp[k++] = e;
+			}
+			Push(p, exp[i]);
+			i++;
+			break;
+		default:
+			while ('0' <= exp[i] && exp[i] <= '9')
+			{
+				postexp[k++] = exp[i];
+				i++;
+			}
+			postexp[k++] = ' ';
+		}
+	}
+	while (!StackEmpty(p))
+	{
+		Pop(p, &e);
+		postexp[k++] = e;
+	}
+	postexp[k++] = '\0';
+}
+
+void comp(const char* exp, int* result)
+{
+	ST* p = InitStack();
+	int num1, num2, e;
+	int i = 0, a;
+	for (; exp[i] != '\0'; i++)
+	{
+		switch (exp[i])
+		{
+		case '+':
+			Pop_num(p, &num1);
+			Pop_num(p, &num2);
+			Push_num(p, num1 + num2);
+			break;
+		case '-':
+			Pop_num(p, &num1);
+			Pop_num(p, &num2);
+			Push_num(p, num2 - num1);
+			break;
+		case '*':
+			Pop_num(p, &num1);
+			Pop_num(p, &num2);
+			Push_num(p, num2 * num1);
+			break;
+		case '/':
+			Pop_num(p, &num1);
+			Pop_num(p, &num2);
+			Push_num(p, num2 / num1);
+			break;
+		default:
+			a = 0;
+			while ('0' <= exp[i] && exp[i] <= '9')
+			{
+				a = a * 10 + exp[i] - '0';
+				i++;
+			}
+			Push_num(p, a);
+		}
+	}
+	Pop_num(p, &e);
+	*result = e;
+	DestroyStack(p);
+}
+
+int main()
+{
+	int a;
+	char ch[MAXSIZE],ch2[MAXSIZE];
+	printf("请输入一个计算式：");
+	scanf("%s",ch);
+	trans(ch, ch2);
+	comp(ch2,&a);
+	printf("%s=%d",ch,a);
+	return 0;
+	// 18 2 +3 *
+}
---
+++ 实验课代码/栈实现的计算器/struct.h
@@ -0,0 +1,131 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<string.h>
+#define ElemType char 
+#define MAXSIZE 100
+#define Status int 
+#define Error 0
+#define True 1
+#define FALSE 0
+#define OK 1
+typedef struct stack {
+	ElemType data[MAXSIZE];
+	int top;
+}ST;
+ST* InitStack()
+{
+	ST* stack = (ST*)malloc(sizeof(ST));
+	stack->top = -1;
+	return stack;
+}
+
+
+Status Push(ST* stack, ElemType e)
+{
+	if (stack->top == MAXSIZE - 1)
+		return FALSE;
+	stack->data[++stack->top] = e;
+	return OK;
+}
+
+
+Status Pop(ST* stack, ElemType* e)
+{
+	if (stack->top == -1)
+		return FALSE;
+	*e = stack->data[stack->top--];
+	return OK;
+}
+
+void DestroyStack(ST* stack)
+{
+	free(stack);
+}
+
+
+int StackEmpty(ST* stack)
+{
+	return (stack->top == -1);
+}
+
+int GetTop(ST* stack, ElemType* e)
+{
+	if (stack->top == -1)
+		return Error;
+	*e = stack->data[stack->top];
+	return True;
+}
+
+void show(ST* stack)
+{
+	int i = 0;
+	for (; i <= stack->top; i++)
+	{
+		printf("%c", stack->data[i]);
+	}
+}
+
+void NumToChar(int a, char* ch)
+{
+	char ch1[MAXSIZE];
+	int j = 0;
+	while (a)
+	{
+		ch1[j++] = a % 10 + '0';
+		a /= 10;
+	}
+	ch1[j] = '\0';
+	int k = 0;
+	for (; k < strlen(ch1); k++)
+	{
+		ch[k] = ch1[strlen(ch1) - 1 - k];
+	}
+	ch[k++] = ' ';
+	ch[k] = '\0';
+}
+
+Status Pop_num(ST* stack, int* e)
+{
+	if (stack->top == -1)
+		return FALSE;
+	char ch = stack->data[stack->top], ch1[MAXSIZE], ch2[MAXSIZE];
+	int flag = 1, a = 0, k = 0, j;
+	while (!StackEmpty(stack))
+	{
+		if (ch == ' ')
+		{
+			if (flag == 1)
+				flag = 0;
+			else
+				break;
+		}
+		else
+		{
+			ch1[k++] = ch;
+		}
+		ch = stack->data[--stack->top];
+	}
+	ch1[k] = '\0';
+	//反转
+	for (j = 0; j < strlen(ch1); j++)
+		ch2[j] = ch1[strlen(ch1) - j - 1];
+	ch2[j] = '\0';
+	//printf("ch2 = %s",ch2);
+	// 字符串转数字
+	for (j = 0; j < strlen(ch2); j++)
+		a = a * 10 + ch2[j] - '0';
+	*e = a;
+	return OK;
+}
+Status Push_num(ST* stack, int e)
+{
+	if (stack->top == MAXSIZE - 1)
+		return FALSE;
+	char ch[200];
+	NumToChar(e, ch);
+	//printf("ch = %s",ch);
+	int i = 0;
+	for (; i < strlen(ch); i++)
+		stack->data[++stack->top] = ch[i];
+	return OK;
+}
