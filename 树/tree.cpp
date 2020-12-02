5a1d12fdc33205cadabebba5f099c4757bc9938a
mayali123 <59428484+mayali123@users.noreply.github.com>
2020-12-02 20:13:12 +0800

    Update tree.cpp

--- 树/tree.cpp
+++ 树/tree.cpp
@@ -7,83 +7,85 @@
 #define MAXSIZE 20
 #define Status int
 #define ElemType char
-typedef struct tree{
+typedef struct tree {
 	ElemType data;
 	struct tree* lchild, * rchild;
 }T;
 typedef struct queue {
-	T data[MAXSIZE];
+	T* data[MAXSIZE];
 	int front, rear;
 }QU;
 typedef struct stack {
 	T data[MAXSIZE];
 	int top;
 }ST;
-void CreateTree(T**tree)
+//需要在函数中改变变量的值，传送变量的指针
+// 需要在函数中改变指针的值，传送指针的指针，某些指针就是变量。
+// 为什么要用 **呢 因为 要修改 指针的值 所以要传递他的地址
+void CreateTree(T** tree)  
 {
 	char e;
-	scanf_s("%c",&e,1);
+	scanf_s("%c", &e, 1);
 	if (' ' == e)
 		*tree = NULL;
 	else
 	{
-		(*tree) =(T*)malloc(sizeof(T));
+		(*tree) = (T*)malloc(sizeof(T));
 		// 先序 
 		(*tree)->data = e;
 		CreateTree(&(*tree)->lchild);
 		CreateTree(&(*tree)->rchild);
 	}
 }
-void show(T*tree)
+void show(T* tree)
 {
 	/*if (!tree)
 		return;*/
 
 	// 配合层次
-	/*QU *q = (QU*)malloc(sizeof(QU));
-	q->front = q->rear = -1;*/
+	QU *q = (QU*)malloc(sizeof(QU));
+	q->front = q->rear = -1;
 
 	ST* st = (ST*)malloc(sizeof(ST));
 	st->top = -1;
 	if (tree)
 	{
-		/*
-		// 先序遍历 
-		printf("%c 在第 %d 层\n",tree->data,level);
-		show(tree->lchild, level + 1);
-		show(tree->rchild, level + 1);
-		*/
-		/*
-			// 中序遍历 
-			show(tree->lchild, level + 1);
-			printf("%c 在第 %d 层\n",tree->data,level);
-			show(tree->rchild, level + 1);
-		*/
-		/*
-			// 后序遍历
-			show(tree->lchild, level + 1);
-			show(tree->rchild, level + 1);
-			printf("%c 在第 %d 层\n",tree->data,level);
-		*/
 		
-		// 层次遍历
-		/*q->data[++q->rear] = *tree;
+		// //先序遍历
+		//printf("%c ",tree->data);
+		//show(tree->lchild );
+		//show(tree->rchild );
+		
+		//// 中序遍历
+		//show(tree->lchild);
+		//printf("%c ",tree->data );
+		//show(tree->rchild);
 
+		//// 后序遍历
+		//show(tree->lchild );
+		//show(tree->rchild );
+		//printf("%c ",tree->data );
+		
+
+		// 层次遍历
+		/*T* t = tree;
+		q->data[++q->rear] = t;
 		while (q->front != q->rear)
 		{
-			*tree=q->data[++q->front];
-			printf("%c ",tree->data);
-			if (tree->lchild)
-				q->data[++q->rear] = *tree->lchild;
-			if (tree->rchild)
-				q->data[++q->rear] = *tree->rchild;
+			t = q->data[++q->front];
+			printf("%c ", t->data);
+			if (t->lchild)
+				q->data[++q->rear] = t->lchild;
+			if (t->rchild)
+				q->data[++q->rear] = t->rchild;
 		}*/
 	}
 }
 int main()
 {
-	T *tree= (T*)malloc(sizeof(T));
+	T* tree = (T*)malloc(sizeof(T));
 	CreateTree(&tree);
 	show(tree);
 	return 0;
 }
+//ab d  ce   
