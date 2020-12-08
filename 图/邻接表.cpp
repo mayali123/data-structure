d8fae84f3acd4ef9ba98348a386037e66259aee7
mayali123 <59428484+mayali123@users.noreply.github.com>
2020-12-08 20:32:37 +0800

    Add files via upload

--- 图/邻接表.cpp
+++ 图/邻接表.cpp
@@ -20,6 +20,12 @@ typedef struct {
 	VNode adjList[MAXV];
 	int numV, numE;
 }GL;
+
+//  队列
+typedef struct {
+	int data[MAXV];
+	int front, rear;
+}queue;
 void CreatALGraph(GL *g)
 {
 	printf("请输入顶点数和边数：");
@@ -62,9 +68,66 @@ void disPAL(GL *g)
 		printf("\n");
 	}
 }
+
+int visit[MAXV];
+
+// 实现 对 g->adjList[v]和其 链接 的 遍历 
+void DFS(GL* g,int v)
+{
+	ENode* p;
+	visit[v] = 1;
+	printf("%c ",g->adjList[v].data);
+	p = g->adjList[v].FristEdge;
+	while (p)
+	{
+		if (visit[p->adjvex]== 0)
+			DFS(g,p->adjvex);
+		p = p->next;
+	}
+}
+void DFSTraver(GL* g)
+{
+	for (int i = 0; i < MAXV; i++)
+		visit[i] = 0;
+	for (int i = 0; i < g->numV; i++)
+		if (visit[i] == 0)
+			DFS(g,i);
+}
+void BFS(GL* g)
+{
+	int visit[MAXV] = { 0 };
+	queue *p = (queue*)malloc(sizeof(queue));
+	p->front = p->rear = 0;
+	for(int i=0;i<g->numV;i++)
+	{
+		if (visit[i] == 0)
+		{
+			visit[i] = 1;
+			p->data[p->front++] = i;
+			while (p->front != p->rear)
+			{
+				int e = p->data[p->rear++];
+				printf("%c ",g->adjList[e].data);
+				ENode* node = g->adjList[e].FristEdge;
+				while (node)
+				{
+					if(visit[node->adjvex]==0)
+					{ 
+						visit[node->adjvex] = 1;
+						p->data[p->front++] = node->adjvex;  // 入队 
+					}
+					node = node->next;
+				}
+			}
+		}
+	}
+}
 int main()
 {
 	GL g;
 	CreatALGraph(&g);
 	disPAL(&g);
+	DFSTraver(&g);
+	printf("\n");
+	BFS(&g);
 }
